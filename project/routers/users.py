61a3e148abfeb0cf824448a0cbe216080b2fae36
AlanCanalesM <alan.canales@micorreo.upp.edu.mx>
2022-07-03 13:36:40 -0500

    I created an endpoint for login

--- project/routers/users.py
+++ project/routers/users.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, HTTPException
 from ..schemas import UserRequestModel
 from ..schemas import UserResponseModel
 from fastapi import APIRouter
+from fastapi.security import HTTPBasicCredentials
 
 
 router = APIRouter(prefix='/users')
@@ -12,7 +13,7 @@ router = APIRouter(prefix='/users')
 async def create_user(user: UserRequestModel):
 
     if User.select().where(User.username == user.username).first():
-        return HTTPException(409, 'This username is already exists, You could try with other')
+        raise HTTPException(409, 'This username is already exists, You could try with other')
 
     hash_password = User.create_password(user.password)
     user = User.create(
@@ -21,3 +22,17 @@ async def create_user(user: UserRequestModel):
     )
 
     return user
+
+@router.post('/login', response_model=UserResponseModel)
+async def login(credentials: HTTPBasicCredentials):
+
+    user = User.select().where(User.username == credentials.username).first()
+
+    if user is None:
+        raise HTTPException(404, "User not found")
+    
+    if user.password != User.create_password(credentials.password):
+        raise HTTPException(404, "Password error")
+
+    return user
+
