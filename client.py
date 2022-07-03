ba06422ab05566a90538f1857099f144554d99a0
AlanCanalesM <alan.canales@micorreo.upp.edu.mx>
2022-07-03 14:35:00 -0500

    Created a client with a get request using request library

---
+++ client.py
@@ -0,0 +1,9 @@
+import requests
+
+URL = 'http://127.0.0.1:8000/api/v1/reviews'
+
+response = requests.get(URL)
+
+if response.status_code == 200:
+    print ('Request success!')
+    print(response.content)
